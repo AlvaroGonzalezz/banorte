59297366389a06ed1a8b9e072a93cfcf61dacb33
AlvaroGonzalezz <alvarogonzalez0509@gmail.com>
2024-09-15 04:52:25 -0600

    Se finalizo la seccion de encuesta

---
+++ scripts.js
@@ -0,0 +1,21 @@
+ function select(btn) {
+    if (btn == 1) {
+        var btn1 = document.getElementById("1");
+        btn1.classList.add("sombreado");
+    } else if (btn == 2) {
+        var btn1 = document.getElementById("2");
+        btn1.classList.add("sombreado");
+    } else if (btn == 3) {
+        var btn1 = document.getElementById("3");
+        btn1.classList.add("sombreado");
+    } else if (btn == 4) {
+        var btn1 = document.getElementById("4");
+        btn1.classList.add("sombreado");
+    } else if (btn == 5) {
+        var btn1 = document.getElementById("5");
+        btn1.classList.add("sombreado");
+    } else if (btn == 6) {
+        var btn1 = document.getElementById("6");
+        btn1.classList.add("sombreado");
+    }
+}
